b695afb80cc14881e6052b9d52e8964023aab0b2
Вагиз <vagmen27@gmail.com>
2018-11-27 11:39:01 +0300

    Добавил метатег для гугла
    Добавил метатег для гугла

--- components/Layout.js
+++ components/Layout.js
@@ -1,30 +1,25 @@
-import { Fragment } from 'react';
-import Head from 'next/head';
-import { LocaleProvider } from 'antd';
-import ruRU from 'antd/lib/locale-provider/ru_RU';
-import { IconContext } from 'react-icons';
-import Header from './Header';
-import Menu from './Menu';
-import Footer from './Footer';
-import StartProject from './StartProject';
+import { Fragment } from "react";
+import Head from "next/head";
+import { LocaleProvider } from "antd";
+import ruRU from "antd/lib/locale-provider/ru_RU";
+import { IconContext } from "react-icons";
+import Header from "./Header";
+import Menu from "./Menu";
+import Footer from "./Footer";
+import StartProject from "./StartProject";
 
 const Layout = ({ title, menuItem, children }) => (
     <Fragment>
         <Head>
             <title>{title}</title>
-            <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1"
-            />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
             <meta charSet="utf-8" />
-            <link
-                rel="stylesheet"
-                href="//cdnjs.cloudflare.com/ajax/libs/antd/3.10.0/antd.min.css"
-            />
+            <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/antd/3.10.0/antd.min.css" />
+            <meta name="google-site-verification" content="q9j1x33suDN0UKqQQ-k2pa9pMUHrbR3weXe3eDY6mUQ" />
         </Head>
         <Header />
         <LocaleProvider locale={ruRU}>
-            <IconContext.Provider value={{ className: 'react-icons' }}>
+            <IconContext.Provider value={{ className: "react-icons" }}>
                 <Fragment>
                     <Menu menuItem={menuItem} />
                     {children}
