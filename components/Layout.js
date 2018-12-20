99c0a524ba40b29aaa6bfe56f009b1d622cb65db
Вагиз <vagmen27@gmail.com>
2018-12-20 10:16:40 +0300

    Добавил фавикон
    Добавил фавикон

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,13 @@
-import { Fragment } from 'react';
-import Head from 'next/head';
-import { LocaleProvider } from 'antd';
-import ruRU from 'antd/lib/locale-provider/ru_RU';
-import { IconContext } from 'react-icons';
-import Header from './Header';
-import Menu from './Menu';
-import Footer from './Footer';
-import StartProject from './StartProject';
-import { YMInitializer } from 'react-yandex-metrika';
+import { Fragment } from "react";
+import Head from "next/head";
+import { LocaleProvider } from "antd";
+import ruRU from "antd/lib/locale-provider/ru_RU";
+import { IconContext } from "react-icons";
+import Header from "./Header";
+import Menu from "./Menu";
+import Footer from "./Footer";
+import StartProject from "./StartProject";
+import { YMInitializer } from "react-yandex-metrika";
 
 const Layout = ({ title, menuItem, children }) => (
     <Fragment>
@@ -18,12 +18,13 @@ const Layout = ({ title, menuItem, children }) => (
             <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/antd/3.10.0/antd.min.css" />
             <meta name="google-site-verification" content="DTZ5X6H4xmsBhZUVAeNA0iqkBxXuf6pwWPzqXC3LhOY" />
             <meta name="yandex-verification" content="6d5ccb923e12ca16" />
+            <link rel="icon" type="image/x-icon" href="/static/images/logo.ico" />
             {/* <meta name="google-site-verification" content="13s4tTPGhfJIZYmOa_q1xPyiypySCl1Z8wJIzwjTV5w" /> */}
         </Head>
         <Header />
         <YMInitializer accounts={[51360247]} />
         <LocaleProvider locale={ruRU}>
-            <IconContext.Provider value={{ className: 'react-icons' }}>
+            <IconContext.Provider value={{ className: "react-icons" }}>
                 <Fragment>
                     <Menu menuItem={menuItem} />
                     {children}
