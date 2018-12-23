5d402ffcd30f5d0ff8617b74b410db427fcd779a
vagmen <vagmen27@gmail.com>
2018-12-23 23:24:17 +0300

    Добавил начало статьи в JSON
    Добавил начало статьи в JSON

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,15 @@
-import { Fragment } from "react";
-import Head from "next/head";
-import { LocaleProvider } from "antd";
-import ruRU from "antd/lib/locale-provider/ru_RU";
-import { IconContext } from "react-icons";
-import { YMInitializer } from "react-yandex-metrika";
-import Header from "./Header";
-import Menu from "./Menu";
-import StartProject from "./StartProject";
-import { mainMenu } from "../constants/menuData";
+import { Fragment } from 'react';
+import Head from 'next/head';
+import { LocaleProvider } from 'antd';
+import ruRU from 'antd/lib/locale-provider/ru_RU';
+import { IconContext } from 'react-icons';
+import { YMInitializer } from 'react-yandex-metrika';
+import Header from './Header';
+import Menu from './Menu';
+import StartProject from './StartProject';
+import { mainMenu } from '../constants/menuData';
 
-const menuItemData = menuItem => mainMenu.find(item => item.src === menuItem);
+const menuItemData = (menuItem) => mainMenu.find((item) => item.src === menuItem);
 
 const Layout = ({ postData, menuItem, children }) => (
     <Fragment>
@@ -21,17 +21,17 @@ const Layout = ({ postData, menuItem, children }) => (
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <meta name="google-site-verification" content="DTZ5X6H4xmsBhZUVAeNA0iqkBxXuf6pwWPzqXC3LhOY" />
             <meta name="yandex-verification" content="6d5ccb923e12ca16" />
-            <meta name="title" content={postData ? postData.title : menuItemData(menuItem).title || "URMAN"} />
+            <title>{postData ? postData.title : menuItemData(menuItem).title || 'URMAN'} </title>
             <meta
                 name="description"
-                content={postData ? postData.description : menuItemData(menuItem).description || ""}
+                content={postData ? postData.description : menuItemData(menuItem).description || ''}
             />
             {/* <meta name="google-site-verification" content="13s4tTPGhfJIZYmOa_q1xPyiypySCl1Z8wJIzwjTV5w" /> */}
         </Head>
         <Header />
         <YMInitializer accounts={[51360247]} />
         <LocaleProvider locale={ruRU}>
-            <IconContext.Provider value={{ className: "react-icons" }}>
+            <IconContext.Provider value={{ className: 'react-icons' }}>
                 <Fragment>
                     <Menu menuItem={menuItem} />
                     {children}
