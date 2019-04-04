b703cf63b0f495c362fb4d778e27be3d6dd8f0ab
Vagiz <Vagmen27@gmail.com>
2019-04-04 10:26:02 +0300

    Добавил генерацию robots.txt и Sitemap. Закрыл от индексации страницы
    Добавил генерацию robots.txt и Sitemap. Закрыл от индексации страницы

--- server.js
+++ server.js
@@ -1,42 +1,64 @@
-const express = require('express');
-const next = require('next');
+const express = require("express");
+const next = require("next");
 
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
+const path = require("path");
+const robotsOptions = {
+    root: path.join(__dirname, "/static"),
+    headers: {
+        "Content-Type": "text/plain;charset=UTF-8"
+    }
+};
+const sitemapOptions = {
+    root: path.join(__dirname, "/static"),
+    headers: {
+        "Content-Type": "text/xml;charset=UTF-8"
+    }
+};
+const faviconOptions = {
+    root: path.join(__dirname, "/static")
+};
 
 app.prepare()
     .then(() => {
         const server = express();
 
-        server.get('/journal/:id', (req, res) => {
-            const actualPage = '/journal/post';
+        server.get("/robots.txt", (req, res) => res.status(200).sendFile("robots.txt", robotsOptions));
+
+        server.get("/sitemap.xml", (req, res) => res.status(200).sendFile("sitemap.xml", sitemapOptions));
+
+        server.get("/favicon.ico", (req, res) => res.status(200).sendFile("favicon.ico", faviconOptions));
+
+        server.get("/journal/:id", (req, res) => {
+            const actualPage = "/journal/post";
             const queryParams = { id: req.params.id };
             app.render(req, res, actualPage, queryParams);
         });
 
-        server.get('/services/:id', (req, res) => {
-            const actualPage = '/services/' + req.params.id;
+        server.get("/services/:id", (req, res) => {
+            const actualPage = "/services/" + req.params.id;
             const queryParams = { id: req.params.id };
             app.render(req, res, actualPage);
         });
 
-        server.get('/admin/posts/:id', (req, res) => {
-            const actualPage = '/admin/posts/edit';
+        server.get("/admin/posts/:id", (req, res) => {
+            const actualPage = "/admin/posts/edit";
             const queryParams = { id: req.params.id };
             app.render(req, res, actualPage, queryParams);
         });
 
-        server.get('*', (req, res) => {
+        server.get("*", (req, res) => {
             return handle(req, res);
         });
 
-        server.listen(80, (err) => {
+        server.listen(dev ? 3000 : 80, err => {
             if (err) throw err;
-            console.log('> Ready on http://localhost:80');
+            console.log(`> Ready on http://localhost: ${dev ? 3000 : 80}`);
         });
     })
-    .catch((ex) => {
+    .catch(ex => {
         console.error(ex.stack);
         process.exit(1);
     });
