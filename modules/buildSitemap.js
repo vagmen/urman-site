b2256ff8ab21e17b36caeedf7ef5062755e55a69
vagmen <vagmen27@gmail.com>
2019-06-03 23:35:05 +0300

    Изменил скрипт для генерации sitemap

--- modules/buildSitemap.js
+++ modules/buildSitemap.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const glob = require("glob");
 const fs = require("fs");
+// const regions = require("./../constants/regions.js");
 
 const SITE_ROOT = process.env.SITE_ROOT || "http://urman.su";
 const SOURCE = process.env.SOURCE || path.join(__dirname, "..", "pages", "/**/*.js");
@@ -13,10 +14,10 @@ xml += '<?xml version="1.0" encoding="UTF-8"?>';
 xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
 const filteredPages = diskPages.filter(
-    page => !page.match(/\/admin/) && !page.match(/\/agreement/) && !page.match(/\/_error/)
+    (page) => !page.match(/\/admin/) && !page.match(/\/agreement/) && !page.match(/\/_error/)
 );
 
-filteredPages.forEach(page => {
+filteredPages.forEach((page) => {
     if (page.match(/.*\/admin$/)) {
         return false;
     }
@@ -30,10 +31,15 @@ filteredPages.forEach(page => {
     page = page.replace(/.js$/, "");
 
     page = `${SITE_ROOT}${page}`;
+    console.log("page", page);
 
     if (page.match(/.*\/index$/)) {
         page = page.replace(/(.*)\/index$/, "$1");
     }
+    // if (page.match(/.*\/regions\/region$/)) {
+    //     regions.regions.map((region) => console.log("region", region.name));
+    //     return false;
+    // }
     xml += "<url>";
     xml += `<loc>${page}</loc>`;
     xml += `<lastmod>${lastMod}</lastmod>`;
@@ -45,5 +51,7 @@ filteredPages.forEach(page => {
 xml += "</urlset>";
 
 fs.writeFileSync(DESTINATION, xml);
+
+console.log("xml", xml);
 
 console.log(`Wrote sitemap for ${filteredPages.length} pages to ${DESTINATION}`);
