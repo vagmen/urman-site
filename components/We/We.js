f9ae2f4bcf0ffb03da188f2f710854a23f18d10b
vagmen <vagmen27@gmail.com>
2020-05-14 10:01:53 +0300

    Добавлена верстка блока "Кто мы"

---
+++ components/We/We.js
@@ -0,0 +1,75 @@
+import React from "react";
+import styles from "./We.module.css";
+import classNames from "classnames";
+import ReactPlayer from "react-player";
+import ReactMarkdown from "react-markdown";
+import { MdCheckCircle } from "react-icons/md";
+
+const We = ({ statistics, whoAreWe, benefits }) => {
+    console.log("benefits", benefits);
+
+    return (
+        <div className={styles.we}>
+            <div className={styles.who}>
+                <ReactMarkdown source={whoAreWe} />
+                {/* <h2>Кто мы?</h2>
+                <ol className={styles.weList}>
+                    <li>Десукция отражает подзол. При равных условиях потускула экспериментально верифицируема.</li>
+                    <li>Десукция отражает подзол. При равных условиях потускула экспериментально верифицируема.</li>
+                    <li>Десукция отражает подзол. При равных условиях потускула экспериментально верифицируема.</li>
+                    <li>Десукция отражает подзол. При равных условиях потускула экспериментально верифицируема.</li>
+                </ol> */}
+            </div>
+            <div className={styles.stat}>
+                <div className={styles.statGrid}>
+                    {statistics.map((item) => (
+                        <>
+                            <div className={styles.statNumber} key={"count-" + item.id}>
+                                <span>{item.count}</span>
+                            </div>
+                            <div className={styles.statText} key={"description-" + item.id}>
+                                <span>{item.description}</span>
+                            </div>
+                        </>
+                    ))}
+                    {/* <div className={styles.statNumber}>
+                        <span>331</span>
+                    </div>
+                    <div className={styles.statText}>
+                        <span>Разработано и согласовано проектов освоения леса</span>
+                    </div>
+                  */}
+                </div>
+            </div>
+            <div className={styles.video}>
+                <ReactPlayer url="https://www.youtube.com/watch?v=2k_m5qGUgjg" width="auto" />
+            </div>
+            <div className={styles.benefits}>
+                {benefits.map((benefit) => (
+                    <div className={styles.benefit} key={"benefit" + benefit.id}>
+                        <MdCheckCircle className={styles.benefitIcon} size={40} />
+                        <p className={styles.benefitText}>{benefit.description}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+// We.getInitialProps = async function () {
+//     const res = await fetch(API_URL + "/statistikas");
+//     const data = await res.json();
+//     console.log("data", data);
+
+//     const statistics = data.map((item) => ({
+//         id: item.id,
+//         count: item.count,
+//         text: item.text,
+//     }));
+
+//     return {
+//         statistics,
+//     };
+// };
+
+export default We;
--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,10 @@ import Button from "../components/ui/Button.js";
 import FeedbackForm from "../components/FeedbackForm";
 import Services from "../components/Services";
 import { servicesData } from "../constants/menuData";
-import { Component } from "react";
 import styles from "./styles.module.css";
+import We from "../components/We/We.js";
+import { API_URL } from "../constants/settings.js";
+import fetch from "isomorphic-unfetch";
 
 const postData = {
     title: "URMAN - Лесные решения",
@@ -14,295 +16,174 @@ const postData = {
         "Берем на себя юридические, проектные, бюрократические вопросы при оформлении и использовании лесного участка.",
 };
 
-// export default () => (
-class Index extends Component {
-    componentDidMount() {
-        // const src = "images/comanda.webp";
-        // let img = document.createElement("img");
-        // img.onload = function () {
-        //     const im = document.querySelector(".main-section__bg");
-        //     im.style.backgroundImage = "url(" + src + ")"; // устанавливаем картинку как фон
-        // };
-        // img.src = src;
+const Index = ({ statistics, whoAreWe, benefits }) => (
+    <Layout postData={postData} headerOpacity={true} isMainPage={true}>
+        <div className="main-container">
+            <section className={styles.bg}></section>
+            {/* <We statistics={statistics} whoAreWe={whoAreWe} benefits={benefits} /> */}
+            <section className="section section__services">
+                <h2>Услуги</h2>
+                <p>Выполним Вам за лучшую цену</p>
+                <Services items={servicesData} />
+                <span className="section__services-button centered">
+                    <Button>
+                        <Link href="/services" passHref>
+                            <a href="">Все услуги</a>
+                        </Link>
+                    </Button>
+                </span>
+            </section>
+            <section className="section section__feedback">
+                <FeedbackForm
+                    title="Напишите свой вопрос"
+                    subTitle="Мы бесплатно проконсультируем Вас по любому вопросу, связанному с оформлением и использованием лесного участка"
+                    backgroundImage="/images/4.jpg"
+                    withComment={true}
+                />
+            </section>
+        </div>
+        <style jsx>{`
+            @keyframes appearance-of-text {
+                from {
+                    opacity: 0;
+                    transform: translateY(200px);
+                }
+                to {
+                    opacity: 0.8;
+                    transform: translateY(0px);
+                }
+            }
+            @keyframes appearance-of-text-delay-1 {
+                from {
+                    opacity: 0;
+                    transform: translateY(200px);
+                }
+                33% {
+                    opacity: 0;
+                    transform: translateY(200px);
+                }
+                to {
+                    opacity: 0.8;
+                    transform: translateY(0px);
+                }
+            }
+            @keyframes appearance-of-text-delay-2 {
+                from {
+                    opacity: 0;
+                    transform: translateY(200px);
+                }
+                50% {
+                    opacity: 0;
+                    transform: translateY(200px);
+                }
+                to {
+                    opacity: 0.8;
+                    transform: translateY(0px);
+                }
+            }
+            .main-container {
+                display: grid;
+                grid-auto-rows: 100vh auto 1fr auto;
+                grid-auto-columns: 1fr;
+                grid-template-areas:
+                    "m"
+                    "s"
+                    "f";
+            }
+            h2 {
+                font-size: 30px;
+            }
+            .section__services {
+                padding: 8px 8px 32px;
+            }
+            .section__services-title {
+                grid-area: sst;
+                text-align: center;
+                color: rgba(0, 0, 0, 0.8);
+                padding: 100px 50px 50px;
+                font-weight: 900;
+            }
+            .section__services-button {
+                grid-area: ssb;
+                text-align: center;
+                padding: 16px;
+            }
+            .section__feedback {
+                grid-area: f;
+                z-index: 1;
+            }
+            @media (min-width: 640px) {
+                h2 {
+                    font-size: 33px;
+                    line-height: 33px;
+                }
+            }
+
+            @media (min-width: 960px) {
+                h2 {
+                    font-size: 36px;
+                    line-height: 36px;
+                }
+            }
+            @media (min-width: 1200px) {
+                h2 {
+                    font-size: 39px;
+                    line-height: 39px;
+                }
+                .section__services {
+                    padding: 32px 16px;
+                }
+            }
+            @media (min-width: 1366px) {
+                h2 {
+                    font-size: 42px;
+                    line-height: 42px;
+                }
+            }
+            @media (min-width: 1600px) {
+                h2 {
+                    font-size: 45px;
+                    line-height: 45px;
+                }
+            }
+            @media (min-width: 1920px) {
+                h2 {
+                    font-size: 48px;
+                    line-height: 48px;
+                }
+            }
+        `}</style>
+    </Layout>
+);
+
+Index.getInitialProps = async function () {
+    let statistics = [];
+    let whoAreWe = "";
+    let benefits = [];
+    try {
+        const res = await fetch(API_URL + "/statistics");
+        const data = await res.json();
+
+        statistics = data.map((item) => ({
+            id: item.id,
+            count: item.count,
+            description: item.description,
+        }));
+
+        const mainJson = await fetch(API_URL + "/main");
+        const main = await mainJson.json();
+        whoAreWe = main.whoAreWe;
+
+        const benefitsJson = await fetch(API_URL + "/benefits");
+        benefits = await benefitsJson.json();
+    } catch (error) {
+        console.log(error);
     }
+    return {
+        statistics,
+        whoAreWe,
+        benefits,
+    };
+};
 
-    render() {
-        return (
-            <Layout postData={postData} headerOpacity={true} isMainPage={true}>
-                <div className="main-container">
-                    {/* <section className="section section__main">
-                        <div className="main-section__bg" />
-                        <div className="main-section__inner">
-                            <h1>С 2010 года упрощаем жизнь нашим клиентам</h1>
-                            <h3>
-                                Берем на себя юридические, проектные, бюрократические вопросы при оформлении и
-                                использовании лесного участка
-                            </h3>
-                            <span className="for-desktop main-section__button">
-                                <ButtonBorder>
-                                    <Link href="/about" passHref>
-                                        <a href="">Подробнее</a>
-                                    </Link>
-                                </ButtonBorder>
-                            </span>
-                        </div>
-                    </section> */}
-
-                    <section className={styles.bg}>
-                        {/* <div className={styles.wrapper}>
-                            <p className={styles.name}>URMAN</p>
-                            <p className={styles.lastName}>лесные решения</p>
-                        </div> */}
-                    </section>
-                    <section className="section section__services">
-                        <h2>Услуги</h2>
-                        <p>Выполним Вам за лучшую цену</p>
-                        <Services items={servicesData} />
-                        <span className="section__services-button centered">
-                            <Button>
-                                <Link href="/services" passHref>
-                                    <a href="">Все услуги</a>
-                                </Link>
-                            </Button>
-                        </span>
-                    </section>
-                    <section className="section section__feedback">
-                        <FeedbackForm
-                            title="Напишите свой вопрос"
-                            subTitle="Мы бесплатно проконсультируем Вас по любому вопросу, связанному с оформлением и использованием лесного участка"
-                            backgroundImage="/images/4.jpg"
-                            withComment={true}
-                        />
-                    </section>
-                </div>
-                <style jsx>{`
-                    @keyframes appearance-of-text {
-                        from {
-                            opacity: 0;
-                            transform: translateY(200px);
-                        }
-                        to {
-                            opacity: 0.8;
-                            transform: translateY(0px);
-                        }
-                    }
-                    @keyframes appearance-of-text-delay-1 {
-                        from {
-                            opacity: 0;
-                            transform: translateY(200px);
-                        }
-                        33% {
-                            opacity: 0;
-                            transform: translateY(200px);
-                        }
-                        to {
-                            opacity: 0.8;
-                            transform: translateY(0px);
-                        }
-                    }
-                    @keyframes appearance-of-text-delay-2 {
-                        from {
-                            opacity: 0;
-                            transform: translateY(200px);
-                        }
-                        50% {
-                            opacity: 0;
-                            transform: translateY(200px);
-                        }
-                        to {
-                            opacity: 0.8;
-                            transform: translateY(0px);
-                        }
-                    }
-                    .main-container {
-                        display: grid;
-                        grid-auto-rows: 100vh 1fr auto;
-                        grid-auto-columns: 1fr;
-                        grid-template-areas:
-                            "m"
-                            "s"
-                            "f";
-                    }
-                    .section__main {
-                        grid-area: m;
-                        display: flex;
-                        flex-wrap: wrap;
-                        justify-content: center;
-                        align-items: center;
-                        position: relative;
-                        text-align: center;
-                    }
-                    .main-section__bg {
-                        position: absolute;
-                        top: 0;
-                        left: 0;
-                        height: 100%;
-                        width: 100%;
-                        background: #eee;
-                        // background: url("static/images/comanda.webp");
-                        background-position: center center;
-                        background-repeat: no-repeat;
-                        background-size: cover;
-                        filter: brightness(0.4);
-                        z-index: -1;
-                    }
-                    h1 {
-                        font-size: 40px;
-                    }
-                    h2 {
-                        font-size: 30px;
-                    }
-                    h3 {
-                        font-size: 20px;
-                    }
-                    .main-section__inner h1,
-                    .main-section__inner h3 {
-                        color: #fff;
-                        opacity: 0.8;
-                    }
-                    .main-section__inner h1 {
-                        margin-top: 110px;
-                        animation: appearance-of-text 1s ease-out;
-                    }
-                    .main-section__inner h3 {
-                        margin-top: 30px;
-                        animation: appearance-of-text-delay-1 1.5s ease-out;
-                    }
-                    .main-section__button {
-                        animation: appearance-of-text-delay-2 2s ease-out;
-                    }
-                    .section__services {
-                        padding: 8px 8px 32px;
-                    }
-                    .section__services-title {
-                        grid-area: sst;
-                        text-align: center;
-                        color: rgba(0, 0, 0, 0.8);
-                        padding: 100px 50px 50px;
-                        font-weight: 900;
-                    }
-                    .section__services-button {
-                        grid-area: ssb;
-                        text-align: center;
-                        padding: 16px;
-                    }
-                    .section__feedback {
-                        grid-area: f;
-                        z-index: 1;
-                    }
-                    @media (min-width: 640px) {
-                        h1 {
-                            font-size: 44px;
-                            line-height: 44px;
-                        }
-                        h2 {
-                            font-size: 33px;
-                            line-height: 33px;
-                        }
-                        h3 {
-                            font-size: 22px;
-                            line-height: 24px;
-                        }
-                        .main-section__inner {
-                            padding: 0 100px;
-                        }
-                    }
-
-                    @media (min-width: 960px) {
-                        h1 {
-                            font-size: 48px;
-                            line-height: 48px;
-                        }
-                        h2 {
-                            font-size: 36px;
-                            line-height: 36px;
-                        }
-                        h3 {
-                            font-size: 24px;
-                            line-height: 28px;
-                        }
-                        .main-section__inner {
-                            padding: 0 200px;
-                        }
-                    }
-                    @media (min-width: 1200px) {
-                        h1 {
-                            font-size: 52px;
-                            line-height: 52px;
-                        }
-                        h2 {
-                            font-size: 39px;
-                            line-height: 39px;
-                        }
-                        h3 {
-                            font-size: 26px;
-                            line-height: 32px;
-                        }
-                        .main-section__inner {
-                            padding: 0 300px;
-                        }
-                        .main-section__inner h1 {
-                            margin-top: 200px;
-                        }
-                        .section__services {
-                            padding: 32px 16px;
-                        }
-                    }
-                    @media (min-width: 1366px) {
-                        h1 {
-                            font-size: 56px;
-                            line-height: 56px;
-                        }
-                        h2 {
-                            font-size: 42px;
-                            line-height: 42px;
-                        }
-                        h3 {
-                            font-size: 28px;
-                            line-height: 36px;
-                        }
-                        .main-section__inner {
-                            padding: 0 350px;
-                        }
-                    }
-                    @media (min-width: 1600px) {
-                        h1 {
-                            font-size: 60px;
-                            line-height: 60px;
-                        }
-                        h2 {
-                            font-size: 45px;
-                            line-height: 45px;
-                        }
-                        h3 {
-                            font-size: 30px;
-                            line-height: 40px;
-                        }
-                        .main-section__inner {
-                            padding: 0 400px;
-                        }
-                    }
-                    @media (min-width: 1920px) {
-                        h1 {
-                            font-size: 64px;
-                            line-height: 64px;
-                        }
-                        h2 {
-                            font-size: 48px;
-                            line-height: 48px;
-                        }
-                        h3 {
-                            font-size: 32px;
-                            line-height: 44px;
-                        }
-                        .main-section__inner {
-                            padding: 0 500px;
-                        }
-                    }
-                `}</style>
-            </Layout>
-        );
-    }
-}
 export default Index;
-// );
